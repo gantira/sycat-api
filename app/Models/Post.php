00245fbf59a7e7fdc9a87f3db383ac4789b044a4
Angga Gantira <angga.gantira@gmail.com>
2021-02-12 15:42:55 +0700

    Setup

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $appends = ['preview_body'];
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getPreviewBodyAttribute()
+    {
+        return Str::words($this->body, 5, ' >>>');
+    }
+}
